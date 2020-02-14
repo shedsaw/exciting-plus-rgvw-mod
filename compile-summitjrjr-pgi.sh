52b222e816e44a569c24a0512307ee676f3c649c
Casey Eichstaedt <ceichsta@vols.utk.edu>
2020-02-13 21:19:14 -0500

    Updated compilation flags for profiling with pgprof

--- compile-summitjrjr-pgi.sh
+++ compile-summitjrjr-pgi.sh
@@ -2,52 +2,56 @@
 
 # Compilers
 export MAKE=make
-export F90=${PGI}/linux86-64/2019/mpi/openmpi-3.1.3/bin/mpif90
+export F90=${PGI}/linux86-64-nollvm/2019/mpi/openmpi-3.1.3/bin/mpif90
 export CUDA_PATH=/usr/local/cuda
 export NVCC=$CUDA_PATH/bin/nvcc
+
+# Copy make.inc
+cp make.inc.local.pgi.cpu make.inc
 
 # Clean up
 make clean
 rm *.o *.mod
-rm bin/elk-cpu bin/elk-gpu
+rm bin/elk-cpu src/elk-cpu
+#rm bin/elk-gpu src/elk-gpu
 
 # Make the binary
-cp make.inc.local.pgi.cpu make.inc
 make
 
 # CPU-only version
 mv src/elk src/elk-cpu
 
 # Compile the necessary codes.
-$NVCC -c -g -G cublas_fortran.cu
-$F90 -Mpreprocess -c -g cublas_fortran_iso.f90
-$F90 -Mpreprocess -g -D_MPI_ -c -I./src/ genmegqblh_cublas.f90
+#$NVCC -c -g -G cublas_fortran.cu
+#$F90 -Mpreprocess -c -g cublas_fortran_iso.f90
+#$F90 -Mpreprocess -g -D_MPI_ -c -I./src/ genmegqblh_cublas.f90
 #$F90 -Mpreprocess -g -D_MPI_ -c -I./src/ genvscrn_cublas.f90
 
 # Move the appropriate files over
-cp genmegqblh_cublas.o src/addons/expigqr/genmegqblh.o
-#cp genvscrn_cublas.o   src/addons/genvscrn.o
-cp cublas_fortran_iso.o cublas_fortran.o cublas_f.mod src/addons/expigqr/
-cp cublas_fortran_iso.o cublas_fortran.o cublas_f.mod src/addons/
-cp cublas_fortran_iso.o cublas_fortran.o cublas_f.mod src/
+#cp genmegqblh_cublas.o src/addons/expigqr/genmegqblh.o
+##cp genvscrn_cublas.o   src/addons/genvscrn.o
+#cp cublas_fortran_iso.o cublas_fortran.o cublas_f.mod src/addons/expigqr/
+#cp cublas_fortran_iso.o cublas_fortran.o cublas_f.mod src/addons/
+#cp cublas_fortran_iso.o cublas_fortran.o cublas_f.mod src/
 
 # Remove main and mod_mpi_grid so they will be recompiled
-rm src/main.o
-#rm src/mod_mpi_grid.mod # Keep old module for pstop()
-rm src/addons/mod_mpi_grid.o
+#rm src/main.o
+##rm src/mod_mpi_grid.mod # Keep old module for pstop()
+#rm src/addons/mod_mpi_grid.o
 
-# re-Make the binary.
-cp make.inc.local.pgi.gpu make.inc
-make
+# copy make.inc and re-Make the binary.
+#cp make.inc.local.pgi.gpu make.inc
+#make
 
 # Copy the hybrid CPU+GPU version
-mv src/elk src/elk-gpu
+#mv src/elk src/elk-gpu
 
 # Keep the two different versions
-make install
-rm bin/elk
+#make install
+#rm bin/elk
+mkdir -p bin
 cd bin
 ln -s -T ../src/elk-cpu elk-cpu
-ln -s -T ../src/elk-gpu elk-gpu
+#ln -s -T ../src/elk-gpu elk-gpu
 cd ..
 
