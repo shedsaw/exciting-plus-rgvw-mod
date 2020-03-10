c130ec1244f7a052d106122c245a96da2eeac7e8
Wileam Phan <wphan@vols.utk.edu>
2020-03-09 20:44:24 -0400

    Cherry-picked hunks of cuda-hydra/7ca103 [^a]; Added install section into some Makefiles [^b]; Added some make.inc files for TAU [^c]
    [^a]: commit 7ca103 from cuda-hydra is
    > Added F90SERIAL into Makefiles`
    
    [^b]: Specifically, the Makefiles for utilities/pp and utilities/spacegroup
    
    [^c]: These are for IBM XL and PGI compilers, but they don't work yet (ongoing OLCF support ticket 419691).
    The [compile script](compile-summit.sh) has been updated, but will quit immediately if the TAU make.inc files are specified.
    
    Conflicts:
    	make.inc.beacon.intel 		(removed)
    	make.inc.beacon.intel.gpu	(removed)
    	make.inc.hydra.gcc.mkl.gpu	(removed)
    	make.inc.local.gcc.cpu  	(modified)
    	utilities/pp/Makefile   	(modified)
    	utilities/spacegroup/Makefile	(modified)

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 
 MAKE = make
+UTILS := spacegroup pp
 
 include make.inc
 
 all:
 	cd src; $(MAKE)
-#	cd src/eos; $(MAKE)
-#	cd src/spacegroup; $(MAKE)
+	for UTIL in $(UTILS); do cd utilities/$$UTIL; $(MAKE); cd ../..; done
 
 clean:
 	cd src; $(MAKE) clean
@@ -14,3 +14,16 @@ clean:
 test:
 	cd tests; ./tests.sh
 
+install:
+	cp src/elk-cpu bin/elk
+	for UTIL in $(UTILS); do cd utilities/$$UTIL; $(MAKE) install; cd ../..; done
+
+docs:
+	[[ -d docs ]] || mkdir docs
+	cd src; $(MAKE) doc; cp elk.pdf ../docs/
+	cd ../utilities/spacegroup; $(MAKE) doc; cp spacegroup.pdf ../../docs/
+	cp src/addons/CRPA-Calculation.pdf docs/
+
+lsvars:
+	echo "export LIBS=\"$(LIBS)\"" > libs.sh; chmod +x libs.sh
+	echo "export F90_OPTS=\"$(F90_OPTS)\"" > f90opts.sh; chmod +x f90opts.sh
--- compile-summit.sh
+++ compile-summit.sh
@@ -7,34 +7,116 @@ export CUDA_PATH=/usr/local/cuda
 export NVCC=$CUDA_PATH/bin/nvcc
 
 # Pick your desired compiler here
-# Options: ibm (default), pgi, gcc, llvm
+# Options: ibm, pgi, gcc, tau-ibm, tau-pgi, tau-gcc
+# TODO: read this as argv[1]
 COMPILER=ibm
 
-# Copy make.inc
-cp make.inc.summit.${COMPILER}.cpu make.inc
-
-# Switch compiler module
-case ${COMPILER} in
-    ibm)
-	module swap xl
-	;;
-    pgi)
-	module swap pgi
-	;;
-    gcc)
-	module swap gcc
-	;;
-    llvm)
-	module swap llvm
-	;;
-    *)
-	echo "Unsupported compiler"
-	exit 1
-esac
-
-# Load modules
+# Load ESSL and HDF5 modules
 module load essl
 module load hdf5
+
+# ESSL depends on libxlf90_r
+# This function extracts IBM XL compiler paths and saves them to xlvars.sh
+getxlvars() {
+  module load xl
+  cat > summit-xlvars.sh << __EOF__
+export OLCF_XL_ROOT=${OLCF_XL_ROOT}
+export OLCF_XLF_ROOT=${OLCF_XLF_ROOT}
+export OLCF_XLC_ROOT=${OLCF_XLC_ROOT}
+export OLCF_XLMASS_ROOT=${OLCF_XLMASS_ROOT}
+export OLCF_XLSMP_ROOT=${OLCF_XLSMP_ROOT}
+__EOF__
+  chmod +x summit-xlvars.sh
+}
+
+# PGI's OpenMP implementation relies on GCC's libatomic
+# This function extracts the GCC compiler path and saves it to xlvars.sh
+getgccvars() {
+  module load gcc
+  echo "export OLCF_GCC_ROOT=${OLCF_GCC_ROOT}" > summit-gccvars.sh
+  chmod +x summit-gccvars.sh
+}
+
+# Load compiler module and TAU (if needed)
+# Note that TAU 2.29 is botched (doesn't link in MPI libs properly)
+# Use tau/2.28.1_patched (but this one doesn't support IBM XL)
+# TODO: Resolve ticket #419691
+case ${COMPILER} in
+  ibm)
+    module load xl
+    ;;
+  pgi)
+    getxlvars
+    getgccvars
+    module load pgi
+    source ./summit-xlvars.sh
+    source ./summit-gccvars.sh
+    ;;
+  gcc)
+    getxlvars
+    module load gcc
+    source ./summit-xlvars.sh
+    ;;
+  llvm)
+    echo "Unsupported compiler (TODO: write make.inc.summit.llvm.cpu)"
+    exit 1
+    #getxlvars
+    #module load llvm
+    #source ./summit-xlvars.sh
+    ;;
+  tau-ibm)
+    echo "Unsupported compiler (see OLCF ticket #419691)"
+    exit 1
+    #module load tau/2.29
+    #module load xl
+    #export TAU_MAKEFILE=${TAU_DIR}/lib/Makefile.tau-xl-papi-mpi-cupti-openmp
+    #export USETAU=1
+    ;;
+  tau-pgi)
+    echo "Unsupported compiler (see OLCF ticket #419691)"
+    exit 1
+    #getxlvars
+    #getgccvars
+    #module load tau/2.28.1_patched
+    #module load pgi
+    #source ./summit-xlvars.sh
+    #source ./summit-gccvars.sh
+    #export TAU_MAKEFILE=${TAU_DIR}/lib/Makefile.tau-pgi-papi-mpi-cupti-pdt-pgi
+    #export USETAU=1
+    ;;
+  tau-gcc)
+    echo "Unsupported compiler (TODO: write make.inc.summit.tau-gcc.cpu)"
+    exit 1
+    #getxlvars
+    #module load tau/2.28.1_patched
+    #module load gcc
+    #source ./summit-xlvars.sh
+    #export TAU_MAKEFILE=${TAU_DIR}/lib/Makefile.tau-gnu-papi-gnu-mpi-cupti-pdt-openmp
+    #export USETAU=1
+    ;;
+  tau-llvm)
+    echo "Unsupported compiler (TODO: write make.inc.summit.tau-llvm.cpu)"
+    exit 1
+    ;;
+  *)
+    echo "Unsupported compiler"
+    exit 1
+esac
+
+# Copy make.inc
+# TODO: Write the unavailable make.inc files
+cp make.inc.summit.${COMPILER}.cpu make.inc
+
+# For now, this is always false
+# TODO: Resolve ticket #419691
+if [ ${USETAU} ]; then
+  source ./summit-xlvars.sh
+  # Get vars from make.inc
+  make lsvars
+  source ./libs.sh
+  # Apply options
+  export TAU_OPTIONS="-optCompInst -optRevert -optTrackIO -optLinking=\"${LIBS}\""
+fi
 
 # Clean up
 make clean
@@ -43,7 +125,7 @@ rm src/elk-cpu bin/elk-cpu
 #rm src/elk-gpu bin/elk-gpu
 
 # Make the binary
-make
+make all
 
 # CPU-only version
 mv src/elk src/elk-cpu
@@ -74,10 +156,8 @@ mv src/elk src/elk-cpu
 # mv src/elk src/elk-gpu
 
 # Keep the two different versions
-# make install # Feature isn't cherry-picked yet from cuda-hydra branch
 [[ -d bin ]] || mkdir bin
 [[ -e bin/elk ]] || rm bin/elk
 cd bin
-ln -s -T ../src/elk-cpu elk-cpu
-#ln -s -T ../src/elk-gpu elk-gpu
+cp ../src/elk-cpu elk-cpu-${COMPILER}
 cd ..
